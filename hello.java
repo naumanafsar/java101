d236e56bd48fe042a77b9a07778260aef960ac5b
Mr <p176053@nu.edu.pk>
2018-02-28 19:32:21 +0500

    Hello World!

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class hello{
+  public static void main(String args[]){
+    System.out.println("Hello world!");
+    System.out.println(args[0]);  //Takes Parameters from Command Line
+  }
+}
